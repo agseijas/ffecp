5b1c6c49b204bfb9ec7f8f4d31c93689b999c87e
Andres Seijas <andres.seijas@lastminute.com>
2018-12-12 09:43:06 +0100

    Introduces Airport class to perform distance calculations

--- src/main/ts/app/app.ts
+++ src/main/ts/app/app.ts
@@ -1,9 +1,28 @@
-export type Coordinate = {altitude: number, latitude: number, longitude: number}
-export type CitiesLocations = {one: Coordinate, other: Coordinate}
+export type GPSCoordinate = {latitude: number, longitude: number, altitude: number}
 
-export class DistanceCalculator {
-    public getDistanceFor(locations : CitiesLocations): number {
-        
-        return 0
+export interface GPSLocation {
+    distanceTo(other: GPSCoordinate): number
+}
+
+export class Airport implements GPSLocation{
+    location: GPSCoordinate
+
+    constructor(location: GPSCoordinate) {
+        console.log(location)
+        this.location = location
+    }
+
+    public distanceTo(other: GPSCoordinate): number {
+        return distanceTwoLocations(this.location, other)
     }
 }
+
+function distanceTwoLocations(one:GPSCoordinate, other:GPSCoordinate) : number{
+    let R = 6371; // earth's mean radius in km
+    let dLat = (one.latitude-other.latitude) * (Math.PI/180);
+    let dLon = (one.longitude-other.longitude) * (Math.PI/180);
+    let latInRad = one.latitude * (Math.PI/180), otherLatInRad = other.latitude * (Math.PI/180);
+    let a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(latInRad) * Math.cos(otherLatInRad) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
+ }
--- src/test/ts/base.spec.ts
+++ src/test/ts/base.spec.ts
@@ -1,15 +1,17 @@
-import { CitiesLocations } from './../../main/ts/app/app';
-import { DistanceCalculator } from "../../main/ts/app/app"
+import { Airport } from './../../main/ts/app/app';
 
 describe("calc milleage for GPS coordinates", () => {
-    const calculator = new DistanceCalculator()
+    let MAD_AIRPORT_GPS_LOCATION = {latitude: 40.492222222, longitude: -3.5716666667, altitude: 1}
+    let CDG_AIRPORT_GPS_LOCATION = {latitude: 49.00972222, longitude: 2.54861111, altitude: 1};
 
-    test("Same city", () => {
-        const sameCity = { 
-            one: {altitude: 1, latitude: 1, longitude: 1}, 
-            other: {altitude: 1, latitude: 1, longitude: 1}
-        }
 
-        expect(calculator.getDistanceFor(sameCity)).toBe(0);
+    test("Same location", () => {
+        let aLocation = new Airport(MAD_AIRPORT_GPS_LOCATION)
+        expect(aLocation.distanceTo(MAD_AIRPORT_GPS_LOCATION)).toBe(0);
+    });
+
+    test("Madrid Paris distance same altitude", () => {
+        let MAD = new Airport(MAD_AIRPORT_GPS_LOCATION)
+        expect(MAD.distanceTo(CDG_AIRPORT_GPS_LOCATION)).toBeCloseTo(1062.4, 1);
     });
 })
