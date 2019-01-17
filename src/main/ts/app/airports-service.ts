773d9a4c409864d2586299d79d7813bfa876f025
Andres Seijas <andres.seijas@lastminute.com>
2019-01-17 13:27:52 +0100

    Enhances typing in airports loading

--- src/main/ts/app/airports-service.ts
+++ src/main/ts/app/airports-service.ts
@@ -8,6 +8,8 @@ interface AirportService {
     loadAsync(seedAirports: AirportLoadCallback): void
 }
 
+type IATAAirport = {code: string, alt: string, lat: string, lon: string}
+
 export class FileSystemCSVAirportsService implements AirportService{
     private filePath: string;
 
@@ -21,15 +23,8 @@ export class FileSystemCSVAirportsService implements AirportService{
         createReadStream(this.filePath, {encoding: 'utf8'})
             .pipe(parser)
             .on('data', record => {
-                const code = record[1] as String
-                const alt = record[13] as number
-                const lat = record[14] as number
-                const lon = record[15] as number
-                if(code !== 'N/A' && !(lat == 0 && lon == 0)){
-                    const adaptedGPSLocation = adapt([code,alt,lat,lon]);
-                    //allAirports += iataGPSLocations + '\n'
-                    airports.set(code, new Airport(adaptedGPSLocation))
-                }
+                const airport = {code: record[1], alt: record[13], lat: record[14], lon: record[15]};
+                store(airport, airports);
             })
             .on('finish', () => {
                 seedAirports(airports)
@@ -37,9 +32,16 @@ export class FileSystemCSVAirportsService implements AirportService{
     }
 }
 
-function adapt(record: any) {
-    const alt = Number.parseFloat(record[1]);
-    const lat = Number.parseFloat(record[2]);
-    const lon = Number.parseFloat(record[3]);
+function store(airport: IATAAirport, airports: Map<String, Airport>) {
+    const iataAirport = adapt(airport);
+    if (airport.code !== 'N/A' && !(iataAirport.latitude == 0 && iataAirport.longitude == 0)) {
+        airports.set(airport.code, new Airport(iataAirport));
+    }
+}
+
+function adapt(airport: IATAAirport) {
+    const alt = Number.parseFloat(airport.alt);
+    const lat = Number.parseFloat(airport.lat);
+    const lon = Number.parseFloat(airport.lon);
     return {altitude: alt, latitude: lat, longitude: lon};
 }
